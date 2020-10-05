f1e3167640405806dad17d3ead6ad6e9ee7ef23b
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-10-05 20:36:43 +0300

    Tutorial started

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const config = require("./config/database");
+
+// Connect to MongoDB
+mongoose.connect(config.database, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// On connection
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected, database => " + config.database);
+});
+
+// On error
+mongoose.connection.on("error", (err) => {
+  console.log("Database error: " + err);
+});
+
+const app = express();
+
+const users = require("./routes/users");
+
+// CORS middleware
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Express JSON parser
+app.use(express.json());
+
+app.use("/users", users);
+
+// Index route
+app.get("/", (req, res) => {
+  res.send("Invalid endpoint!");
+});
+
+// Port number
+const port = 3000;
+
+// Start server
+app.listen(port, () => {
+  console.log("Server started on port " + port);
+});
